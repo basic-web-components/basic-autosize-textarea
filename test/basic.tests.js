2fb536c2bdda3215cc919d76d7cd867e0d56dc93
Rob Bearman <robbear@hyperfine.com>
2015-05-12 11:18:41 -0700

    Update from consolidated repo - advancing beyond 0.5.0

--- test/basic.tests.js
+++ test/basic.tests.js
@@ -4,8 +4,52 @@ suite('basic', function() {
 
   var container = document.getElementById('container');
 
-  teardown(function () {
-    container.innerHTML = '';
+  function setInnerHTML(elem, text) {
+    var textNode = document.createTextNode(text);
+    var children = Polymer.dom(elem).childNodes;
+    for (var i = 0; i < children.length; i++) {
+      Polymer.dom(elem).removeChild(children[i]);
+    }
+    Polymer.dom(elem).appendChild(textNode);
+  }
+
+  test('instantiation', function(done) {
+    var fixture = document.createElement('basic-autosize-textarea');
+    container.appendChild(fixture);
+    flush(function() {
+      assert(fixture);
+      done();
+    });
+  });
+
+  test('innerHTML becomes value', function(done) {
+    container.innerHTML = '<basic-autosize-textarea>Hello</basic-autosize-textarea>';
+    var fixture = container.querySelector('basic-autosize-textarea');
+    flush(function() {
+      assert.equal(fixture.value, 'Hello');
+      done();
+    });
+  });
+
+  test('set innerHTML', function(done) {
+    var fixture = document.createElement('basic-autosize-textarea');
+    container.appendChild(fixture);
+    var content = 'Initial value';
+    fixture.value = content;
+    flush(function() {
+      assert.equal(fixture.value, content);
+      var newContent = 'New value';
+      if (fixture.shadowRoot) {
+        fixture.innerHTML = newContent;
+      }
+      else {
+        setInnerHTML(fixture, newContent);
+      }
+      flush(function() {
+        assert.equal(fixture.value, newContent);
+        done();
+      });
+    });
   });
 
   test('default minimumRows', function(done) {
@@ -19,10 +63,25 @@ suite('basic', function() {
 
   test('set minimumRows', function(done) {
     var fixture = document.createElement('basic-autosize-textarea');
+    var fixtureComp = document.createElement('basic-autosize-textarea');
     container.appendChild(fixture);
+    container.appendChild(fixtureComp);
     fixture.minimumRows = 10;
     flush(function() {
       assert.equal(fixture.minimumRows, 10);
+      assert(fixture.$.textBox.clientHeight > 2 * fixtureComp.$.textBox.clientHeight);
+      done();
+    });
+  });
+
+  test('set minimumRows declaratively', function(done) {
+    container.innerHTML = '<basic-autosize-textarea minimum-rows="10">Hello</basic-autosize-textarea>';
+    var fixture = container.querySelector('basic-autosize-textarea');
+    var fixtureComp = document.createElement('basic-autosize-textarea');
+    container.appendChild(fixtureComp);
+    flush(function() {
+      assert.equal(fixture.minimumRows, 10);
+      assert(fixture.$.textBox.clientHeight > 2 * fixtureComp.$.textBox.clientHeight);
       done();
     });
   });
@@ -30,9 +89,10 @@ suite('basic', function() {
   test('value', function(done) {
     var fixture = document.createElement('basic-autosize-textarea');
     container.appendChild(fixture);
-    fixture.innerHTML = 'Hello, world';
+    fixture.value = 'Hello, world';
     flush(function() {
       assert.equal(fixture.value, "Hello, world");
+      assert(fixture.$.textBox.clientHeight > 1);
       done();
     })
   });
@@ -42,28 +102,33 @@ suite('basic', function() {
     var fixtureComp = document.createElement('basic-autosize-textarea');
     container.appendChild(fixture);
     container.appendChild(fixtureComp);
-    fixture.minimumRows = 10;
-    fixture.innerHTML = 'Hello, world';
     flush(function() {
-      // Confirm minimumRows can make the element taller than is required just to fit the content
-      assert.equal(fixture.minimumRows, 10);
-      assert.equal(fixture.value, 'Hello, world');
-      assert(fixture.$.textBox.clientHeight > 2 * fixtureComp.$.textBox.clientHeight);
-      done();
+      var content = 'Hello world';
+      fixture.minimumRows = 10;
+      fixture.value = content;
+      flush(function() {
+        // Confirm minimumRows can make the element taller than is required just to fit the content
+        assert.equal(fixture.minimumRows, 10);
+        assert.equal(fixture.value, content);
+        assert(fixture.$.textBox.clientHeight > 2 * fixtureComp.$.textBox.clientHeight);
+        done();
+      });
     });
   });
 
-  test('value through innerHTML', function(done) {
+  test('value changes', function(done) {
     var fixture = document.createElement('basic-autosize-textarea');
     container.appendChild(fixture);
     var content = 'Hello, world';
-    fixture.innerHTML = content;
+    fixture.value = content;
     flush(function() {
       assert.equal(fixture.value, content);
-      var newContent = "Changed content";
-      fixture.innerHTML = newContent;
+      assert(fixture.$.textBox.clientHeight > 1);
+      var newContent = 'Changed content';
+      fixture.value = newContent;
       flush(function() {
         assert.equal(fixture.value, newContent);
+        assert(fixture.$.textBox.clientHeight > 1);
         done();
       });
     });
@@ -75,7 +140,8 @@ suite('basic', function() {
     container.appendChild(fixture);
     container.appendChild(fixtureComp);
     // Confirm autosize works when content is HTML
-    fixture.value = '<html>\n<body>\n<p>\nThis is a test\n</p>\n<div>\nSome more tests\n</div>\n</body>\n</html>';
+    var html = '<html>\n<body>\n<p>\nThis is a test\n</p>\n<div>\nSome more tests\n</div>\n</body>\n</html>';
+    fixture.value = html;
     flush(function() {
       assert(fixture.$.textBox.clientHeight > 2 * fixtureComp.$.textBox.clientHeight);
       done();
@@ -87,12 +153,12 @@ suite('basic', function() {
     var fixtureComp = document.createElement('basic-autosize-textarea');
     container.appendChild(fixture);
     container.appendChild(fixtureComp);
-    fixture.value = 'Lots of words to force wrapping. Lots of words to force wrapping. ' +
+    var text = 'Lots of words to force wrapping. Lots of words to force wrapping. ' +
     'Lots of words to force wrapping. Lots of words to force wrapping. Lots of words to force wrapping. ' +
     'Lots of words to force wrapping. Lots of words to force wrapping. Lots of words to force wrapping. ' +
     'Lots of words to force wrapping. Lots of words to force wrapping. Lots of words to force wrapping. ' +
     'Lots of words to force wrapping. Lots of words to force wrapping. Lots of words to force wrapping. ';
-
+    fixture.value = text;
     flush(function() {
       assert(fixture.$.textBox.clientHeight > 2 * fixtureComp.$.textBox.clientHeight);
       done();
@@ -104,8 +170,8 @@ suite('basic', function() {
     var fixtureComp = document.createElement('basic-autosize-textarea')
     container.appendChild(fixture);
     container.appendChild(fixtureComp);
-    fixture.value = 'sadf;lksadfjl;sdfjl;ksjdfkjsd;lfjsdlkfjksdjfosadjflwemrflkwefoawefowmefomwefomowefomweofmwqefmwoqemfowqefomwqefoiwemfomwqeofkmwefomwerfklasjdlmwefoqwkemfwqemfowqmefowqmefowmefowqemfowemfowqemfwoefokwqmefokmwerfokmwefwoqemfowqmefokqwmefowqefowqmefoqwemfowemfomweofijweoirulwemfkwejfoijewtjoiqhewfknwerofijqewrotmerfomoewfqweofmqweofjqweofmweomwqeorjqweoifmqwoefqoewfmoqewfmqwoefmqwefoqwemfoiwejrtomqwdfoiqwemromqweofiwmerowqeoimwqeormwqefom';
-
+    text = 'sadf;lksadfjl;sdfjl;ksjdfkjsd;lfjsdlkfjksdjfosadjflwemrflkwefoawefowmefomwefomowefomweofmwqefmwoqemfowqefomwqefoiwemfomwqeofkmwefomwerfklasjdlmwefoqwkemfwqemfowqmefowqmefowmefowqemfowemfowqemfwoefokwqmefokmwerfokmwefwoqemfowqmefokqwmefowqefowqmefoqwemfowemfomweofijweoirulwemfkwejfoijewtjoiqhewfknwerofijqewrotmerfomoewfqweofmqweofjqweofmweomwqeorjqweoifmqwoefqoewfmoqewfmqwoefmqwefoqwemfoiwejrtomqwdfoiqwemromqweofiwmerowqeoimwqeormwqefom';
+    fixture.value = text;
     flush(function() {
       assert(fixture.$.textBox.clientHeight > 2 * fixtureComp.$.textBox.clientHeight);
       done();
@@ -124,4 +190,3 @@ suite('basic', function() {
   });
 
 });
-
